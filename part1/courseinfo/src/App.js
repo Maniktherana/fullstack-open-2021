2598791725f8a0e8a84504b98266ab084a426410
Manik <manikrana54@gmail.com>
2021-06-11 14:44:55 +0530

    Add part1
    Initial commit

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Header = () => {
+
+}
+
+const Content = () => {
+  
+}
+
+const Total = () => {
+  
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
