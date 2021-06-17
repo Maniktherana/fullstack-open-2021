3b84c65e805d007ba8f7873dc27c5733edf9234e
Manik <manikrana54@gmail.com>
2021-06-17 19:50:38 +0530

    Add countrydata step 1

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -1,9 +1,61 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Result = ({ countriesToShow, find }) => {
+  if (find === '') {
+    return (<div></div>)
+  } else if(countriesToShow.length > 10) {
+    return (
+      <div>
+        Too many matches, specift another filter
+      </div>
+    )
+  } else if(countriesToShow.length < 11 && countriesToShow.length !== 1) {
+    return (
+      <div>
+        <ul>
+          {countriesToShow.map(name =>
+                  <Name key={name.name} name={name}/>
+                  )}
+        </ul>
+      </div>
+    )
+  } else if(countriesToShow.length === 1) {
+    return (
+      <div>
+          {countriesToShow.map(name =>
+                  <Data key={name.name} name={name}/>
+                  )}
+      </div>
+    )
+  } 
+}
+
 const Data = ({ name }) => {
   return (
-    <li>{name.name}</li>
+    <div>
+      <h1>{name.name}</h1>
+      <div>capital: {name.capital}</div>
+      <div>population: {name.population}</div>
+      <h2>Languages</h2>
+      <ul>
+        {name.languages.map(lang =>
+                <Langauges key={lang.name} lang={lang}/>)}
+      </ul>
+      <img src={name.flag} alt={`${name.name} flag`}></img>
+    </div>
+  )
+}
+
+const Langauges = ({ lang }) => {
+  return (
+    <li>{lang.name}</li>
+  )
+}
+
+const Name = ({ name }) => {
+  return (
+    <div>{name.name}</div>
   )
 }
 
@@ -11,9 +63,8 @@ const App = () => {
   
   const [countries, setCountries] = useState([])
   const [find, setFind] = useState('')
-  const [result, setResult] = useState([])
 
-  const countriesToShow = countries
+  const countriesToShow = countries.filter(name => name.name.toLowerCase().includes(find.toLowerCase()))
 
   const hook = () => {
     console.log('effect')
@@ -26,25 +77,23 @@ const App = () => {
   }
   
   useEffect(hook, [])
-  console.log(countriesToShow)
 
   const handleFindChange = (event) => {
     setFind(event.target.value)
     console.log(find)
   }
 
+  console.log(countriesToShow)
+
   return (
     <div>
-      find countries: <input
-          value={find}
-          onChange={handleFindChange}/>
       <div>
-        <ul>
-          {countriesToShow.map(name =>
-                <Data key={name.alpha3code} name={name}/>
-                )}
-        </ul>
+        find countries: <input
+            value={find}
+            onChange={handleFindChange}/>
       </div>
+      
+      <Result countriesToShow={countriesToShow} find={find}/>
     </div>
   )
 }
