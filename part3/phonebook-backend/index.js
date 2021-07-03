c21973d39b76ccd320b19033ac59403d29ae108d
Manik Rana <manikrana54@gmail.com>
2021-07-03 20:48:56 +0530

    heroku app

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
-
 
 let persons = [
     { 
@@ -95,7 +96,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
   
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
