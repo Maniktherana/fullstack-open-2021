a8eb84ca27d4bb6808d36b9f1101753c299f9bd5
Manik Rana <manikrana54@gmail.com>
2021-07-03 17:12:35 +0530

    Add phonebook-backend step3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -40,6 +40,18 @@ app.get('/info', (request, response) => {
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+  
   
 const PORT = 3001
 app.listen(PORT, () => {
