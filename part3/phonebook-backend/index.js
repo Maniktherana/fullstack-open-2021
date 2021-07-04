e75f670df2ff21736a9bc3b8495e6b45c75a1466
Manik Rana <manikrana54@gmail.com>
2021-07-04 13:47:25 +0530

    Add phonebook database step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -56,22 +56,17 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
-    })
-  })
-
-// app.get('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     console.log(id)
-//     const person = persons.find(person => person.id === id)
-//     if (person) {
-//         response.json(person)
-//       } else {
-//         response.status(404).end()
-//       }
-// })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+      .then(person => {
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+  })  
 
 app.post('/api/persons', morgan(':body'), (request, response) => {
     const body = request.body
@@ -100,14 +95,6 @@ app.post('/api/persons', morgan(':body'), (request, response) => {
 
     morgan.token('body', function (req) {return JSON.stringify(req.body)})
 
-    // const person = {
-    //     id: generateId(),
-    //     name: body.name,
-    //     number: body.number,
-    // }
-
-    // persons = persons.concat(person)
-    // response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -116,15 +103,26 @@ app.delete('/api/persons/:id', (request, response, next) => {
         response.status(204).end()
       })
       .catch(error => next(error))
-  })
+})
 
-// app.delete('/api/persons/:id', (request, response) => {
-//   const id = Number(request.params.id)
-//   persons = persons.filter(person => person.id !== id)
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
 
-//   response.status(204).end()
-// })
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
 
+app.use(errorHandler)
   
 const PORT = process.env.PORT
 app.listen(PORT, () => {
