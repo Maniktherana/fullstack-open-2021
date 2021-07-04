6aa18a87bd1f079236ad9bd86fce48189d1349bd
Manik Rana <manikrana54@gmail.com>
2021-07-04 15:56:25 +0530

    Add lint configuration

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
@@ -13,24 +15,24 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+        'id': 1,
+        'name': 'Arto Hellas', 
+        'number': '040-123456'
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+        'id': 2,
+        'name': 'Ada Lovelace', 
+        'number': '39-44-5323523'
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+        'id': 3,
+        'name': 'Dan Abramov', 
+        'number': '12-43-234345'
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+        'id': 4,
+        'name': 'Mary Poppendieck', 
+        'number': '39-23-6423122'
     }
 ]
 
@@ -54,22 +56,22 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
-      .then(person => {
-        if (person) {
-          response.json(person)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
-  })  
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})  
 
 app.post('/api/persons', morgan(':body'), (request, response, next) => {
     const body = request.body
@@ -77,7 +79,7 @@ app.post('/api/persons', morgan(':body'), (request, response, next) => {
 
     if ((body.name === undefined || !body.number === undefined)) {
         return response.status(400).json({ 
-        error: 'content missing' 
+            error: 'content missing' 
         })
     }
 
@@ -95,7 +97,7 @@ app.post('/api/persons', morgan(':body'), (request, response, next) => {
     person.save()
         .then(savedPerson => savedPerson.toJSON())
         .then(savedAndFormattedPerson => {
-        response.json(savedAndFormattedPerson)
+            response.json(savedAndFormattedPerson)
         }) 
         .catch(error => next(error))
 
@@ -114,16 +116,16 @@ app.put('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndUpdate(request.params.id, person, { new: true })
         .then(updatedPerson => {
             response.json(updatedPerson)
-            })
+        })
         .catch(error => next(error))    
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -149,5 +151,5 @@ app.use(errorHandler)
   
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 require('dotenv').config()
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
@@ -8,7 +10,7 @@ console.log('connecting to', url)
 
 const password = process.argv[2]
 
-  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(result => {
         console.log('connected to MongoDB')
     })
@@ -22,22 +24,22 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         require: true,
         unique: true
-        },
+    },
     number: {
         type: String,
         minLength: 8,
         require: true,
         unique: true
-        }
+    }
 })
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
+})
 
 module.exports = mongoose.model('Person', personSchema)
