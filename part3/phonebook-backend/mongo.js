29e4f33fc06b4d544f7487ea52f30bcdb38697f5
Manik Rana <manikrana54@gmail.com>
2022-01-16 03:42:38 -0500

    refactor code

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -10,11 +11,11 @@ const password = process.argv[2]
 const url =
   `mongodb+srv://fullstackManik1:${password}@cluster0.wejhw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
-  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+    name: String,
+    number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -23,25 +24,26 @@ if(process.argv[3]){
     const name = process.argv[3]
     const number = process.argv[4]
     const person = new Person({
-      name: name,
-      number: number
+        name: name,
+        number: number
     })
   
+    // eslint-disable-next-line no-unused-vars
     person.save().then(response => {
-      mongoose.connection.close()
-      console.log(`added ${name} ${number} to phonebook`)
+        mongoose.connection.close()
+        console.log(`added ${name} ${number} to phonebook`)
     })
-  }
+}
   
-  else{
+else{
     
     Person.find({}).then(result => {
-      console.log('phonebook')
-      result.forEach(person => {
-        var s =  `${person.name} ${person.number}`
-        console.log(s)
-      })
-      mongoose.connection.close()
+        console.log('phonebook')
+        result.forEach(person => {
+            var s =  `${person.name} ${person.number}`
+            console.log(s)
+        })
+        mongoose.connection.close()
     })
   
-  }
+}
